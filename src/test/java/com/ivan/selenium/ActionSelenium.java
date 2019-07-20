0d9b58d035d07173462b7606e9bac4771c9fcd5d
Ivan Zhong <ivanzhong0310@gmail.com>
2019-07-21 00:42:45 +1200

    RadioBox-click,clear,isSelected

--- src/test/java/com/ivan/selenium/ActionSelenium.java
+++ src/test/java/com/ivan/selenium/ActionSelenium.java
@@ -2,7 +2,10 @@ package com.ivan.selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class ActionSelenium {
 
@@ -26,6 +29,55 @@ public class ActionSelenium {
         driver.findElement(By.name("email")).clear();
         String s = driver.findElement(By.name("email")).getAttribute("placeholder");
         System.out.println(s);
+
+        //preparing for radioBox
+        driver.findElement(By.name("email")).sendKeys("ivanzhong0310@gmail.com");
+        driver.findElement(By.name("password")).sendKeys("aaaaaaaa");
+        driver.findElement(By.className("moco-btn")).click();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 单选框
+     * radioBox
+     */
+    public void radioBox(){
+        driver.get("https://www.imooc.com/user/setprofile");
+        driver.findElement(By.className("pull-right")).click();
+        //driver.findElement(By.xpath("/html/body/div[9]/div/div[2]/div/div/form/div[4]/div/label[1]")).click();
+        List<WebElement> elements = driver.findElements(By.xpath("/html/body/div[9]/div/div[2]/div/div/form/div[4]/div/label//input"));
+        System.out.println(elements.size());
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (WebElement radio : elements){
+            boolean flag = radio.isSelected();
+            if (flag == false){
+                radio.click();
+                break;
+            }else{
+                System.out.println("isSelected");
+            }
+        }
+
+
+
+
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void main(String[] args){
@@ -33,5 +85,6 @@ public class ActionSelenium {
         ActionSelenium actionSelenium = new ActionSelenium();
         actionSelenium.InitDriver();
         actionSelenium.inputBox();
+        actionSelenium.radioBox();
     }
 }
